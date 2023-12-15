428538e0921f7c0ecb5b8d6b8c0b7ca5f9662bfa
prajwalgowda900 <gprajwal822@gmail.com>
2023-12-15 21:49:53 +0530

    test

---
+++ Two.js
@@ -0,0 +1,14 @@
+let ids=[101,102,103,104]
+//create new array using for of 
+//create new array using array map
+//create new array using .../spread op
+let new_Ids = [...ids]
+/* let new_Ids=ids.map((id)=>{
+    return id
+})
+ */
+/* for( id of ids){
+    new_Ids.push(id)
+} */
+console.log(ids)
+console.log(new_Ids)
