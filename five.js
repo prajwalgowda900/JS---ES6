9ceb6a96cd0d6010cf958b1a3d7ed8359127b66f
prajwalgowda900 <gprajwal822@gmail.com>
2023-12-16 15:18:04 +0530

    okie'

---
+++ five.js
@@ -0,0 +1,12 @@
+let users={
+    name:"rahul",
+    email:"rahul@gmailcom",
+    loc:'bangalore'
+}
+let details={
+    avail:false,
+    email:"rahul@ibm.com",
+    sal:45000
+}
+let user_Details={...users,...details}
+console.log(user_Details)
---
+++ four.js
@@ -0,0 +1,5 @@
+let enames=['Rahul','sonai','priyanka']
+
+let new_Enames=[...enames,'modi','shaw']
+
+console.log(new_Enames)
