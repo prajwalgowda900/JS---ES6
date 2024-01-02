1ba4f2004a9400b108697dd8ec7344260780a912
Prajwal Gowda <142891668+prajwalgowda900@users.noreply.github.com>
2024-01-02 20:12:26 +0530

    Add files via upload

---
+++ es6/Srtingliteril.js
@@ -0,0 +1,11 @@
+let empid=101
+let ename="Rahul"
+console.log("Employee Id:", empid, ",Employee Name",ename)
+console.log(`Employee Id:${empid}, Employee Name: ${ename}`)
+
+/*
+backtick,
+What is string literal/literal string
+-read variable in the form of string
+-to construct url - we are using string literal
+*/
