6ad6de38f552b91f3f770643aef5fc10e9f19d66
Prajwal Gowda <142891668+prajwalgowda900@users.noreply.github.com>
2024-01-11 22:22:58 +0530

    Add files via upload

---
+++ es6/NewMethods.js
@@ -0,0 +1,18 @@
+let ids=[101,102,103,104]
+
+ids.map(()=>{
+    console.log("GM")
+})
+console.log("*********************")
+ids.forEach(()=>{
+    console.log("GM")
+})
+console.log("*********************")
+ids.filter(()=>{
+    console.log("GM")
+})
+console.log("*********************")
+ids.reduce(()=>{
+    console.log("GM")
+})
+console.log("*********************")
